07906fbab34455e59a3e9d585170f55de3ec744d
Unia909 <78199030+unia909@users.noreply.github.com>
2023-01-28 22:28:35 +0300

    Add files via upload

--- win32u.h
+++ win32u.h
@@ -8,6 +8,11 @@
  * Name in library: NtUserEnableMouseInputForCursorSuppression
  * Parameter bShow: TRUE to show cursor and FALSE to hide cursor
  * Returns: TRUE if success and FALSE if fail (Watch GetLastError())
+ * Remarks: for the function to work, after call the application
+ *          must create some kind of window, as when using
+ *          MessageBox or create an invisible window using
+ *          CreateWindow even without registering its own window
+ *          class and message loop
  */
 typedef BOOL(*NtUserEnableMouseInputForCursorSuppression_Function)(BOOL bShow);
 
