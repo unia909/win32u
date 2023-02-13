e64947436b2a756d3715bc32102c288c57dfdd62
Unia909 <78199030+unia909@users.noreply.github.com>
2023-02-13 17:45:45 +0300

    Add alternative functions name

--- win32u.h
+++ win32u.h
@@ -1,26 +1,32 @@
-#ifndef LOGONUI_WIN32U_H
-#define LOGONUI_WIN32U_H
+#ifndef WIN32U_DEF_H
+#define WIN32U_DEF_H
 
 #include <minwindef.h>
 
 /*
  * DLL Library: win32u.dll
  * Name in library: NtUserEnableMouseInputForCursorSuppression
+ * Alternative DLL: user32.dll
+ * Alternative name: Ordinal 2519
+ * Available starting from Windows 8 (NT 6.2)
  * Parameter bShow: TRUE to show cursor and FALSE to hide cursor
  * Returns: TRUE if success and FALSE if fail (Watch GetLastError())
  * Remarks: for the function to work, after call the application
  *          must create some kind of window, as when using
- *          MessageBox or create an invisible window using
- *          CreateWindow even without registering its own window
- *          class and message loop
+ *          MessageBox or create an invisible window even
+ *          without registering its own window class and
+ *          message loop
  */
 typedef BOOL(*NtUserEnableMouseInputForCursorSuppression_Function)(BOOL bShow);
 
 /*
  * DLL Library: win32u.dll
  * Name in library: NtUserIsMouseInputEnabled
+ * Alternative DLL: user32.dll
+ * Alternative name: Ordinal 2520
+ * Available starting from Windows 8 (NT 6.2)
  * Returns: TRUE if success and FALSE if fail (Watch GetLastError()) (It may fail?)
  */
 typedef BOOL(*NtUserIsMouseInputEnabled_Function)();
 
-#endif //LOGONUI_WIN32U_H
+#endif //WIN32U_DEF_H
